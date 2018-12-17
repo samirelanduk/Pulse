175b77c97ddddd4330562c95b294ea658043d1ab
Sam Ireland <sam.ireland.uk@gmail.com>
2018-12-17 14:24:45 +0000

    Hackathon time and description

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,21 +1,21 @@
-/**	
+/**
 	* Template Name: Eventoz
-	* Version: 1.0	
+	* Version: 1.0
 	* Template Scripts
 	* Author: MarkUps
 	* Author URI: http://www.markups.io/
 
 	Custom JS
-	
+
 	1. FIXED MENU
 	2. EVENT TIME COUNTER
 	3. MENU SMOOTH SCROLLING
 	4. VIDEO POPUP
 	5. SPEAKERS SLIDEER ( SLICK SLIDER )
-	6. BOOTSTRAP ACCORDION 
-	7. MOBILE MENU CLOSE  
-	
-	
+	6. BOOTSTRAP ACCORDION
+	7. MOBILE MENU CLOSE
+
+
 **/
 
 
@@ -32,7 +32,7 @@
 	jQuery(window).bind('scroll', function () {
     if ($(window).scrollTop() > 150) {
         $('.mu-navbar').addClass('mu-nav-show');
-        
+
 	    } else {
 	        $('.mu-navbar').removeClass('mu-nav-show');
 	    }
@@ -41,8 +41,8 @@
 	/* ----------------------------------------------------------- */
 	/*  2. EVENT TIME COUNTER
 	/* ----------------------------------------------------------- */
-	
-	$('#mu-event-counter').countdown('2020/02/10').on('update.countdown', function(event) {
+
+	$('#mu-event-counter').countdown('2019/01/27 09:00').on('update.countdown', function(event) {
 	  var $this = $(this).html(event.strftime(''
 	    + '<span class="mu-event-counter-block"><span>%D</span> Days</span> '
 	    + '<span class="mu-event-counter-block"><span>%H</span> Hours</span> '
@@ -50,10 +50,10 @@
 	    + '<span class="mu-event-counter-block"><span>%S</span> Secs</span>'));
 	});
 
-	
+
     /* ----------------------------------------------------------- */
 	/*  3. MENU SMOOTH SCROLLING
-	/* ----------------------------------------------------------- */ 
+	/* ----------------------------------------------------------- */
 
 	 //MENU SCROLLING WITH ACTIVE ITEM SELECTED
 
@@ -74,7 +74,7 @@
 	menuItems.click(function(e){
 	  var href = $(this).attr("href"),
 	      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+22;
-	  jQuery('html, body').stop().animate({ 
+	  jQuery('html, body').stop().animate({
 	      scrollTop: offsetTop
 	  }, 1500);
 	  e.preventDefault();
@@ -84,7 +84,7 @@
 	jQuery(window).scroll(function(){
 	   // Get container scroll position
 	   var fromTop = $(this).scrollTop()+topMenuHeight;
-	   
+
 	   // Get id of current scroll item
 	   var cur = scrollItems.map(function(){
 	     if ($(this).offset().top < fromTop)
@@ -93,40 +93,40 @@
 	   // Get the id of the current element
 	   cur = cur[cur.length-1];
 	   var id = cur && cur.length ? cur[0].id : "";
-	   
+
 	   if (lastId !== id) {
 	       lastId = id;
 	       // Set/remove active class
 	       menuItems
 	         .parent().removeClass("active")
 	         .end().filter("[href=\\#"+id+"]").parent().addClass("active");
-	   }           
+	   }
 	})
 
 
-	
+
 	/* ----------------------------------------------------------- */
 	/*  4. VIDEO POPUP
 	/* ----------------------------------------------------------- */
 
    $('.mu-video-play-btn').on('click', function(event) {
-	   
+
         event.preventDefault();
-        
+
         $('.mu-video-iframe-area').addClass('mu-video-iframe-display');
-       
+
     });
-   
+
     // when click the close btn
 
     // disappear iframe window
-    
+
     $('.mu-video-close-btn').on('click', function(event) {
-	    
+
 	    event.preventDefault();
-	    
+
 		$('.mu-video-iframe-area').removeClass('mu-video-iframe-display');
-		
+
     });
 
     // stop iframe if it is play while close the iframe window
@@ -140,18 +140,18 @@
     // when click overlay area
 
      $('.mu-video-iframe-area').on('click', function(event) {
-	    
+
 	    event.preventDefault();
-	    
+
 		$('.mu-video-iframe-area').removeClass('mu-video-iframe-display');
-		
+
     });
 
 	$('.mu-video-iframe-area, .mu-video-iframe').on('click', function(e){
 	    e.stopPropagation();
 	});
 
-		
+
 	/* ----------------------------------------------------------- */
 	/*  5. SPEAKERS SLIDEER ( SLICK SLIDER )
 	/* ----------------------------------------------------------- */
@@ -176,29 +176,29 @@
 		  ]
 		});
 
-		
+
 
 
 
 	/* ----------------------------------------------------------- */
-	/*  6. BOOTSTRAP ACCORDION 
-	/* ----------------------------------------------------------- */ 
+	/*  6. BOOTSTRAP ACCORDION
+	/* ----------------------------------------------------------- */
 
 		/* Start for accordion #1*/
 		$('#accordion .panel-collapse').on('shown.bs.collapse', function () {
 		$(this).prev().find(".fa").removeClass("fa-angle-up").addClass("fa-angle-down");
 		});
-		
+
 		//The reverse of the above on hidden event:
-		
+
 		$('#accordion .panel-collapse').on('hidden.bs.collapse', function () {
 		$(this).prev().find(".fa").removeClass("fa-angle-down").addClass("fa-angle-up");
 		});
 
 
 	/* ----------------------------------------------------------- */
-	/*  7. MOBILE MENU CLOSE 
-	/* ----------------------------------------------------------- */ 
+	/*  7. MOBILE MENU CLOSE
+	/* ----------------------------------------------------------- */
 
 	jQuery('.mu-menu').on('click', 'li a', function() {
 	  $('.mu-navbar .in').collapse('hide');
@@ -208,11 +208,6 @@
 
 
 
-	
-	
+
+
 })( jQuery );
-
-
-
-  
-	
