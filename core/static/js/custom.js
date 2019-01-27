664633a4a6cae298db3f42a12c672a2d64d0edb3
Sam Ireland <sam.ireland.uk@gmail.com>
2019-01-27 14:16:41 +0000

    Fix date

--- core/static/js/custom.js
+++ core/static/js/custom.js
@@ -42,7 +42,7 @@
 	/*  2. EVENT TIME COUNTER
 	/* ----------------------------------------------------------- */
 
-	$('#mu-event-counter').countdown('2019/01/27 09:00').on('update.countdown', function(event) {
+	$('#mu-event-counter').countdown('2019/01/26 09:00').on('update.countdown', function(event) {
 	  var $this = $(this).html(event.strftime(''
 	    + '<span class="mu-event-counter-block"><span>%D</span> Days</span> '
 	    + '<span class="mu-event-counter-block"><span>%H</span> Hours</span> '
